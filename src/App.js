a0d9d64224f95fc505277cdf821d80d69d623355
Alqama Bin Sadiq <alqamabinsadiq@gmail.com>
2019-09-13 16:08:46 +0500

    Modify App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import StreamingBox from "./components/streaming_box/StreamingBox.index";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  return <StreamingBox />;
+};
 
 export default App;
